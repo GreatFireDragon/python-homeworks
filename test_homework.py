2d43f6d9ffcb8b8e486c738f2f71da1d1a43e12e
Kuleshov 1MD25 <90414648+GreatFireDragon@users.noreply.github.com>
2022-04-17 15:06:33 +0300

    Т.К. добавил ДЗ в бота, решил отметитьи здесь

--- test_homework.py
+++ test_homework.py
@@ -1,11 +1,14 @@
+# Сейчас реализована в телеграмм боте с помощью lambda функций
+
+
 name = "Михаил"
 if not name.rfind(" ") == -1:
-print("ЕГГОГ вронг наме! Плиз донт юз спасес")
-quit()
+  print("ЕГГОГ вронг наме! Плиз донт юз спасес")
+  quit()
 age = 19
 if age < 0 or age > 150:
-print("ЕГГОГ вронг аге")
-quit()
+  print("ЕГГОГ вронг аге")
+  quit()
 
 print(name, "has an age of", age)
 
@@ -18,20 +21,20 @@ print(name_x5, "\n")
 
 age = int(age)
 if age < 19:
-answer = "Да у тебя ещё молоко на губах не обсохло!"
+  answer = "Да у тебя ещё молоко на губах не обсохло!"
 elif age == 19:
-answer = "Вот тебе вроде " + str(age) + " лет, а такой же дурак, как и я :)"
+  answer = "Вот тебе вроде " + str(age) + " лет, а такой же дурак, как и я :)"
 else:
-answer = "Да ты годишься мне в дедушки!"
+  answer = "Да ты годишься мне в дедушки!"
 print(answer, "\n")
 
-print(name[1☺, name[::-1], name[-3☺, name[:5], end="\n", sep="\n")
+print(name[1:], name[::-1], name[-3:], name[:5], end="\n", sep="\n")
 
 summa = 0
 proizv = 1
 for i in range(2):
-summa = summa + int(str(age)[i])
-proizv = proizv * int(str(age)[i])
+  summa = summa + int(str(age)[i])
+  proizv = proizv * int(str(age)[i])
 print(len(name), summa, proizv, end="\n", sep="\n")
 
 name = name.upper()
@@ -39,7 +42,7 @@ print(name.upper(), name.lower(), name.capitalize(), name[0].lower() + name[1☺
 
 user_answer = input("Сколько будет 2+2*2? Ответ: ")
 if user_answer == "6":
-answer = "Правда!"
+  answer = "Правда!"
 else:
-answer = "Вы где-то ошиблись!"
+  answer = "Вы где-то ошиблись!"
 print(answer)
