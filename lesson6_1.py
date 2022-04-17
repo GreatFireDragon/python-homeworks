a98349240073c651c76f7879dc59d94341e25702
greatfiredragon <kulmike2002@gmail.com>
2022-04-17 15:48:27 +0300

    Цщклштп

--- lesson6_1.py
+++ lesson6_1.py
@@ -1,5 +1,26 @@
 # ...
 import argparse
+import os
 
-ans = "sdjgsdjklgjs"
+ans = "Working...\n--------------------------------------\n"
+print(ans)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--name', default='Anonimous', help="Имя пользователя")
+parser.add_argument('-p', '--path', help="Путь к файлу изображения")
+parser.add_argument('-noQ', '--noQuest', action='store_true', help="Подавление вопросов пользователю")
+args = parser.parse_args()
+
+# print(f'Hi, {args.name}!')
+print(f'Текущие параметры: {str(vars(args))[1:-1]}')
+
+
+
+
+
+
+
+
+
+ans = "\n--------------------------------------\n...End of work"
 print(ans)
