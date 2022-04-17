5af69279125a72899f03eb62b9c8ec81e18d2de1
greatfiredragon <kulmike2002@gmail.com>
2022-04-17 16:38:41 +0300

    Работа над 2м заданием

--- lesson6_2.py
+++ lesson6_2.py
@@ -1,37 +1,83 @@
 # ...
 import argparse
 import os
+import configparser
 
-ans = "Working...\n--------------------------------------\n"
+
+ans = "Start of programm...\n--------------------------------------"
 print(ans)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-n', '--name', default='Anonimous', help="Имя пользователя")
-parser.add_argument('-p', '--path', default='', help="Путь к файлу изображения")
-parser.add_argument('-noQ', '--noQuest', action='store_true', help="Подавление вопросов пользователю")
+
+
+class Cartoon:
+    def __init__(self, name, genre, origin, main_character):
+        self.name = name
+        self.genre = genre
+        self.origin = origin
+        self.main_character = main_character
+
+    def __str__(self):
+        return f"Cartoon's name: {self.name} \nGenre: {self.genre} \nOrigin: {self.origin} \nMain character_s: {self.main_character}"
+
+    def __getstate__(self) -> dict:
+        state = {}
+        state["name"] = self.name
+        state["genre"] = self.genre
+        state["origin"] = self.origin
+        state["main_character"] = self.main_character
+        return state
+
+    def __setstate__(self, state: dict):
+        self.name = state["name"]
+        self.genre = state["genre"]
+        self.origin = state["origin"]
+        self.main_character = state["main_character"]
+
+
+parser = argparse.ArgumentParser(description='Parser. Just a parser.')
+parser.add_argument('-f', '--file_s_name', default='default.ini', nargs='?')
+parser.add_argument('-p', '--path', default='Settings\\', help="Here's the path to your file")
 args = parser.parse_args()
+print(f'Текущие параметры: {str(vars(args))[1:-1]}', end='\n\n')
 
-# print(f'Hi, {args.name}!')
-print(f'Привет, {args.name}!')
-# print(f'Текущие параметры: {str(vars(args))[1:-1]}', end='\n\n')
+config_path = args.path + args.file_s_name
+config = configparser.ConfigParser()
+config.read(config_path, encoding="UTF-8")
 
-print(f'Проверяю путь к изображению: {args.path}...')
-if os.path.exists(args.path):
-    succes = f'Файл {os.path.basename(args.path)} успешно удалён!'
+print(config["GIT"]["TOKEN"])
 
-    if args.noQuest:
-        os.remove(args.path)
-        print(succes)
-    else:
-        ans = input(f'Удалить файл {os.path.basename(args.path)}?    Y/N: ').capitalize()
-        if ans[0] == 'Y':
-            os.remove(args.path)
-            print(succes)
-        else:
-            pass
+
+print(config_path)
+if os.path.exists(config_path):
+    print("\nФайлы на месте.\n")
+    pass
 else:
-    print(f'Путь {args.path} не существует.')
+    print("\nПроверьте наличие папки Settings.\nОна должна находиться в корне и содержать файл default.ini\n"
+          "Если файлы прсиутствуют, а ошибка не исчезла, скорее всего файлы повреждены.")
 
 
-ans = "\n--------------------------------------\n...End of work"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ans = "\n--------------------------------------\n...End of programm"
 print(ans)
