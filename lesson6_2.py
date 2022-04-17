c583f2d0c6f91f7e6869024fabeaf601c7c477e8
greatfiredragon <kulmike2002@gmail.com>
2022-04-17 17:11:24 +0300

    ДЗ сделано

--- lesson6_2.py
+++ lesson6_2.py
@@ -2,79 +2,65 @@
 import argparse
 import os
 import configparser
-
-
-ans = "Start of programm...\n--------------------------------------"
-print(ans)
+import datetime
 
 
 
-class Cartoon:
-    def __init__(self, name, genre, origin, main_character):
-        self.name = name
-        self.genre = genre
-        self.origin = origin
-        self.main_character = main_character
+
+class Simple_Game:
+    def __init__(self, level: int, hp: int, time_played: int):
+        self.level = level
+        self.hp = hp
+        self.time_played = time_played
 
     def __str__(self):
-        return f"Cartoon's name: {self.name} \nGenre: {self.genre} \nOrigin: {self.origin} \nMain character_s: {self.main_character}"
+        return f"Уровень, на котором находится игрок: {self.level} \n" \
+               f"Количество здоровья игрока: {self.hp} \n" \
+               f"Время в игре: {self.time_played} \n"
 
-    def __getstate__(self) -> dict:
-        state = {}
-        state["name"] = self.name
-        state["genre"] = self.genre
-        state["origin"] = self.origin
-        state["main_character"] = self.main_character
-        return state
+def game_builder():
+    config = configparser.ConfigParser()
+    config.read(config_path, encoding="UTF-8")
 
-    def __setstate__(self, state: dict):
-        self.name = state["name"]
-        self.genre = state["genre"]
-        self.origin = state["origin"]
-        self.main_character = state["main_character"]
+    return Simple_Game(
+        int(config["SAVE" + str(save_number)]["level"]),
+        int(config["SAVE" + str(save_number)]["hp"]),
+        int(config["SAVE" + str(save_number)]["time_played"])
+    )
 
 
-parser = argparse.ArgumentParser(description='Parser. Just a parser.')
-parser.add_argument('-f', '--file_s_name', default='default.ini', nargs='?')
-parser.add_argument('-p', '--path', default='Settings\\', help="Here's the path to your file")
+
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file_s_name', default='default.ini', nargs='?', help="Файл настроек")
+parser.add_argument('-p', '--path', default='Settings\\', help="Путь к файлу настроек")
+parser.add_argument('-s', '--save', default=1, help="Номер сохранения")
 args = parser.parse_args()
-print(f'Текущие параметры: {str(vars(args))[1:-1]}', end='\n\n')
+print(f'Текущие параметры: {str(vars(args))[1:-1]}', end='\n')
 
 config_path = args.path + args.file_s_name
-config = configparser.ConfigParser()
-config.read(config_path, encoding="UTF-8")
-
-print(config["GIT"]["TOKEN"])
+save_number = args.save
 
 
-print(config_path)
+
+
+
+
+ans = "\nStart of programm...\n--------------------------------------"
+print(ans)
 if os.path.exists(config_path):
     print("\nФайлы на месте.\n")
-    pass
+    save1 = game_builder()
+
+    print(save1.__str__())
+
 else:
-    print("\nПроверьте наличие папки Settings.\nОна должна находиться в корне и содержать файл default.ini\n"
+    print("\nПроверьте наличие папки Settings.\nОна должна находиться в корне и содержать файл default.ini"
+          " и settings.ini\n"
           "Если файлы прсиутствуют, а ошибка не исчезла, скорее всего файлы повреждены.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    exit(0)
 
 
 
